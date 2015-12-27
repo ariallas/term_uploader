fc4715a212816f1f28c3d35d06dbfbb14d4dfc61
Ilya Reznikov <ril.xilus@gmail.com>
2015-12-27 12:01:39 +0300

    Fixed warnings, removed old code

--- gen.py
+++ gen.py
@@ -129,117 +129,10 @@ class SqlTransformer:
             if i not in self.common_data:
                 raise Exception("No {0} found in the document".format(i))
 
-    @staticmethod
-    def get_or_create(table, variable, condition, values):
-        return "select id into {1} from ont.{0} where {2};\n" \
-               "if {1} is NULL then\n" \
-               "\tinsert into ont.{0} values ({3}) returning id into {1};\n" \
-               "end if;\n\n".format(table, variable, condition, values)
-
-    def measure_values(self):
-        sql = ""
-        for i in range(len(self.table)):
-            row = self.table[i]
-            for j in range(len(row)):
-                sql += "\n\t(nextval('points_of_measure_id_seq'), " \
-                       "{0}, " \
-                       "\t{1}, " \
-                       "(select id from data_set), " \
-                       "data_source_id, " \
-                       "(select id from ont.dimensions where dimension_name = '{2}'), " \
-                       "(select id from ont.physical_quantities where quantity_designation = '{3}')),".format(
-                        row[j],
-                        i,
-                        self.table_dimensions[j],
-                        self.table_quantities[j]
-                        )
-        return sql[:-1]
-
-    def uncertainty_values(self):
-        sql = ""
-        for i in range(len(self.table)):
-            row = self.table[i]
-            for j in range(len(row)):
-                sql += "\n\t(nextval('measurement_uncertainties_id_seq'), " \
-                       "'{0}', " \
-                       "nextval('points_of_measure_id_seq_copy'), " \
-                       "(select id from uncertainty_type)),".format(
-                        self.common_data['precision'])
-        return sql[:-1]
-
-    def insert_rows(self, file_name):
-        return "with\n" \
-               "state as (select id from ont.states where lower(state_name) = '{0}'),\n" \
-               "data_set as (insert into ont.data_sets " \
-               "values(nextval('data_sets_id_seq'), '{1}', '{2}', '{3}', substance_in_state_id) returning id)\n" \
-               "insert into ont.points_of_measure values {4};\n\n" \
-               "with uncertainty_type as (select id from ont.uncertainty_types where uncertainty_name = '{5}')\n" \
-               "insert into ont.measurement_uncertainties values {6};\n\n".format(
-                self.common_data['state'],
-                file_name,
-                self.common_data['description'],
-                re.sub('-', '', str(datetime.date.today())),
-                self.measure_values(),
-                self.common_data['uncertainty_name'],
-                self.uncertainty_values())
-
-    def generate_sql1(self, file_name, cursor):
-        xls_reader = XlsReader()
-        self.common_data, self.table, \
-        self.table_quantities, self.table_dimensions, self.table_roles = xls_reader.read_table(file_name)
-        print(self.common_data)
-        print(self.table_quantities)
-        print(self.table_dimensions)
-        print(self.table[0])
-
-        self.check_data()
-
-        sql = "DO $$\n\ndeclare\n" \
-              "\tchemical_substance_id bigint;\n" \
-              "\tsubstance_in_state_id bigint;\n" \
-              "\tdata_source_id        bigint;\n\n" \
-              "begin\n\n"
-
-        sql += self.get_or_create(
-            "chemical_substances",
-            "chemical_substance_id",
-            "chemical_formula = '{1}' or substance_name = '{0}'".format(self.common_data['name'],
-                                                                        self.common_data['formula']),
-            "nextval('chemical_substances_id_seq'), '{1}', '{0}'".format(self.common_data['name'],
-                                                                         self.common_data['formula']))
-        sql += self.get_or_create(
-            "substances_in_states",
-            "substance_in_state_id",
-            "substance_id = chemical_substance_id",
-            "nextval('substances_in_states_id_seq'),"
-            "'id' || chemical_substance_id || '_' || (select id from ont.states where lower(state_name)='{0}') || '_c',"
-            "'id' || chemical_substance_id || '_' || (select id from ont.states where lower(state_name)='{0}') || '_f',"
-            "FALSE,"
-            "chemical_substance_id,"
-            "(select id from ont.states where lower(state_name) = '{0}')".format(self.common_data['state']))
-
-        sql += self.get_or_create(
-            "data_sources",
-            "data_source_id",
-            "data_source_name = '{0}'".format(self.common_data['source']),
-            "nextval('data_sources_id_seq'), '{0}'".format(self.common_data['source']))
-
-        sql += "drop sequence if exists points_of_measure_id_seq_copy;\n" \
-               "create temp sequence points_of_measure_id_seq_copy;\n" \
-               "perform setval('points_of_measure_id_seq_copy', currval('points_of_measure_id_seq'));\n\n"
-
-        sql += self.insert_rows(file_name)
-
-        sql += "\nEND $$\nLANGUAGE plpgsql;"
-
-        script_file = open(re.sub('\.xls.*', '.sql', file_name), mode='w')
-        script_file.write(sql)
-        script_file.close()
-
     def generate_sql(self, file_name, cursor):
         xls_reader = XlsReader()
         self.common_data, self.table, \
-        self.table_quantities, self.table_dimensions, self.table_roles = xls_reader.read_table(file_name)
+            self.table_quantities, self.table_dimensions, self.table_roles = xls_reader.read_table(file_name)
         print(self.common_data)
         print(self.table_quantities)
         print(self.table_dimensions)
@@ -260,14 +153,14 @@ class SqlTransformer:
         # Getting chemical substance
         cursor.execute("select id from ont.chemical_substances "
                        "where chemical_formula = '{0}' or substance_name = '{1}'".format(
-            self.common_data['formula'],
-            self.common_data['name']))
+                        self.common_data['formula'],
+                        self.common_data['name']))
         substance_id = cursor.fetchone()
         if substance_id is None:
             sql += "insert into ont.chemical_substances values " \
                    "(nextval('chemical_substances_id_seq'), '{0}', '{1}');\n".format(
-                self.common_data['formula'],
-                self.common_data['name'])
+                    self.common_data['formula'],
+                    self.common_data['name'])
             substance_id = "currval('chemical_substances_id_seq')"
         else:
             substance_id = substance_id[0]
@@ -279,7 +172,7 @@ class SqlTransformer:
         if source_id is None:
             sql += "insert into ont.data_sources values " \
                    "(nextval('data_sources_id_seq'), '{0}');\n".format(
-                self.common_data['source'])
+                    self.common_data['source'])
             source_id = "currval('data_sources_id_seq')"
         else:
             source_id = source_id[0]
@@ -294,8 +187,8 @@ class SqlTransformer:
         if substance_in_state_id is None:
             sql += "insert into ont.substances_in_states values (nextval('substances_in_states_id_seq'), " \
                    "'id' || {0} || '_' || {1} || '_c','id' || {0} || '_' || {1} || '_f', FALSE, {0} , {1});\n".format(
-                substance_id,
-                state_id)
+                    substance_id,
+                    state_id)
             substance_in_state_id = "currval('substances_in_states_id_seq')"
         else:
             substance_in_state_id = substance_in_state_id[0]
@@ -365,8 +258,8 @@ class SqlTransformer:
             for j in range(len(self.table)):
                 sql += "\n\t(nextval('measurement_uncertainties_id_seq'), {0}, " \
                        "nextval('points_of_measure_id_seq_copy'), {1}),".format(
-                    self.common_data['precision'],
-                    uncertainty_type_id)
+                        self.common_data['precision'],
+                        uncertainty_type_id)
         sql = sql[:-1] + ';\n'
 
         sql += "\ncommit;"
@@ -377,10 +270,10 @@ class SqlTransformer:
 
 connection_file = open('ConnectionString.txt', mode='r')
 connection = psycopg2.connect(connection_file.read())
-cursor = connection.cursor()
+curs = connection.cursor()
 
 st = SqlTransformer()
 for file in glob.glob('./*.xls*'):
     file = file[2:]
     if file[0] != '~':
-        st.generate_sql(file, cursor)
+        st.generate_sql(file, curs)
